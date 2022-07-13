bf1d921e4f781abaa2cbdf8427386738ba73eb38
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-07-13 17:21:10 -0400

    clearing old react logo and statr new look

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,6 @@ function App() {
     .then(res => res.text())
     .then(data => setMessage(data));
   }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This was edited 
-        </a>
-        {message && <p>{message}</p>}
-      </header>
-    </div>
-  );
 }
 
 export default App;
